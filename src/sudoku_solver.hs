4663bdebc8da09a70c72c99202e52f0fda23bb07
Luis Alberto <luis_hernandez@ciencias.unam.mx>
2021-02-14 01:50:11 -0600

    Estableciendo datos del sudoku.

--- src/sudoku_solver.hs
+++ src/sudoku_solver.hs
@@ -13,12 +13,29 @@ data Tree a = Node a [Tree a]
 
 filas = "ABCDEFGHI"
 columnas = "123456789"
+digitos = "123456789"
+area = (('A','1'),('I','9'))
 
 cartesiano :: [a] -> [b] -> [(a, b)]
 cartesiano xs ys = (,) <$> xs <*> ys
 
 celdas :: [Celda]
 celdas = cartesiano filas columnas
+
+unidades :: [Unidad]
+unidades = [cartesiano filas [c] | c <- columnas] ++ [cartesiano [r] columnas | r <- filas] ++
+           [cartesiano rs cs | rs <- ["ABC","DEF","GHI"], cs <- ["123","456","789"]]
+
+unisXcelda :: Array Celda [Unidad]
+unisXcelda = array area [(s, [filter (/= s) u | u <- unidades, s `elem` u ]) | s <- celdas]
+
+posibilidades :: Tablero
+posibilidades = array area [(s, digitos) | s <- celdas]
+
+amigos :: Array Celda [Celda]
+amigos = array area [(c, set (unisXcelda!c)) | c <- celdas]
+  where
+    set = nub . concat
 
 asigna :: Tablero -> (Celda, Valor) -> Maybe Tablero
 asigna t (c, v) = Nothing
@@ -44,12 +61,25 @@ muestraSolucion = putStrLn "hola"
 muestraNumSoluciones :: IO ()
 muestraNumSoluciones = putStrLn "hola"
 
-procesa :: Char -> IO ()
-procesa c = putStrLn "hola"
+obtenValores :: IO ()
+obtenValores = do putStr "hola"
 
 muestraMenu :: IO Char
-muestraMenu = return 'c'
+muestraMenu = do
+    putStrLn "¿Qué desea hacer?"
+    putStrLn "[1] Resolver un Sudoku."
+    putStrLn "[2] Salir."
+    putStr "Opción: "
+    opcion <- getChar
+    return opcion
+
+procesa :: Char -> IO ()
+procesa '1' = obtenValores
+procesa '2' = putStrLn "\n\nSaliendo...\n"
+procesa _ = putStr "\n\nOpción no válida.\n\n"
 
 main :: IO ()
 main = do
-    putStrLn "SUDOKU SOLVER \n"
+    putStrLn "---SUDOKU SOLVER---\n"
+    m <- muestraMenu
+    procesa m
