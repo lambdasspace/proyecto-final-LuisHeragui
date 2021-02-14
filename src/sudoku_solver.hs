b72fc5dd1ba72505962ad5441f8cc12d3d48ba67
Luis Alberto <luis_hernandez@ciencias.unam.mx>
2021-02-14 05:39:01 -0600

    Implementando interacción con usuario y validación.

--- src/sudoku_solver.hs
+++ src/sudoku_solver.hs
@@ -52,18 +52,80 @@ obtenSolucion (Node a []) = a
 obtenNumSoluciones :: Tree Tablero -> Int
 obtenNumSoluciones (Node a []) = 0
 
-agregaValor :: IO ()
-agregaValor = putStrLn "hola"
-
 muestraSolucion :: IO ()
 muestraSolucion = putStrLn "hola"
 
 muestraNumSoluciones :: IO ()
 muestraNumSoluciones = putStrLn "hola"
+-----------------------------Impresión
+ejemplo :: Tablero
+ejemplo = array area [(s, (fst s) : (snd s) : []) | s <- celdas]
 
-obtenValores :: IO ()
-obtenValores = do putStr "hola"
+imprimeTablero :: Tablero -> IO ()
+imprimeTablero = putStrLn . tableroToString
 
+tableroToString :: Tablero -> String
+tableroToString g = "\n" ++ unlines l5
+  where
+    l0 = elems g -- ["12347","2689",...]
+    l1 = map (\s -> " " ++ s ++ " ") l0 -- [" 1 "," 2 ",...]
+    l2 = map concat $ subdivide 3 l1 -- [" 1  2  3 "," 4  5  6 ",...]
+    l3 = subdivide 3 l2 -- [[" 1  2  3 "," 4  5  6 "," 7  8  9 "],...]
+    l4 = map (concat . intersperse "|") l3 -- [" 1  2  3 | 4  5  6 | 7  8  9 ",...]
+    l5 = concat $ intersperse [linea] $ subdivide 3 l4 -- añade líneas divisorias
+    long    = div (maximum $ map length l4) 3
+    guiones = replicate long '-'
+    linea   = guiones ++ "+" ++ guiones ++ "+" ++ guiones
+
+subdivide :: Int -> [String] -> [[String]]
+subdivide n [] = []
+subdivide n xs = ys : subdivide n zs
+  where
+    (ys,zs) = splitAt n xs
+
+-----------------------------Validación y preparación
+celdasValidas = [f : c : [] | f <- filas, c <- columnas]
+
+obtenValores :: [String] -> [(Celda, Valor)] -> Tablero -> IO [(Celda, Valor)]
+obtenValores [] cv muestra = return cv
+obtenValores (c:cs) cv muestra = do
+    putStr $ "\nIngresa un valor entre 1 y 9 para " ++ c ++ ": "
+    x <- getChar
+    if elem x digitos
+        then do
+            let celda = (head c, last c)
+                muestra' = muestra // [(celda, [x, ' '])]
+            putStr "\n"
+            imprimeTablero muestra'
+            obtenValores cs (cv ++ [(celda, x)]) muestra'
+        else do
+            putStrLn "\nPor favor ingrese un valor correcto."
+            obtenValores (c:cs) cv muestra
+
+solicitaCeldas :: IO [(Celda, Valor)]
+solicitaCeldas = do
+    putStrLn "\n¿En qué celdas desea colocar valores?"
+    putStrLn "Ejemplo: A1 C4 D9 I2"
+    imprimeTablero ejemplo
+    putStr "Celdas: "
+    cs <- getLine
+    let cels = words cs
+    if all (`elem` celdasValidas) cels == True
+        then do
+            v <- obtenValores cels [] ejemplo
+            return (actualiza celdas v [])
+        else do
+            putStr "\nPor favor siga el ejemplo e ingrese celdas válidas.\n"
+            solicitaCeldas
+
+actualiza :: [Celda] -> [(Celda, Valor)] -> [(Celda, Valor)] -> [(Celda, Valor)]
+actualiza [] _ _ = []
+actualiza (c:cs) [] xs = [(c, '.')] ++ actualiza cs xs []
+actualiza (c:cs) (cv:cvs) xs
+    | c == fst cv = [cv] ++ actualiza cs (xs ++ cvs) []
+    | otherwise = actualiza (c:cs) cvs (xs ++ [cv])
+
+-----------------------------Menú
 muestraMenu :: IO Char
 muestraMenu = do
     putStrLn "¿Qué desea hacer?"
@@ -74,12 +136,14 @@ muestraMenu = do
     return opcion
 
 procesa :: Char -> IO ()
-procesa '1' = obtenValores
+procesa '1' = do
+    v <- solicitaCeldas
+    putStr "Bien"
 procesa '2' = putStrLn "\n\nSaliendo...\n"
 procesa _ = putStr "\n\nOpción no válida.\n\n"
 
 main :: IO ()
 main = do
-    putStrLn "---SUDOKU SOLVER---\n"
+    putStrLn "\n---SUDOKU SOLVER---\n"
     m <- muestraMenu
     procesa m
